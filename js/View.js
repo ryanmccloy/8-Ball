85b2cc8f1107a625f5c32ec409689b094a393139
Ryan McCloy <ryannmccloy@gmail.com>
2023-08-30 21:08:42 +0300

    updated 8 ball styling and added answers to js file

--- js/View.js
+++ js/View.js
@@ -19,3 +19,26 @@ function randomNumber(min, max) {
 createStars(1, 100);
 createStars(2, 85);
 createStars(3, 70);
+
+// Magic 8-Ball Answers
+
+const answer1 = "It is certain";
+const answer2 = "It is Decidedly So";
+const answer3 = "Without A Doubt";
+const answer4 = "Yes Definitely";
+const answer5 = "You May Rely On it";
+const answer6 = "As I see it, Yes";
+const answer7 = "Most Likely";
+const answer8 = "Outlook Good";
+const answer9 = "Yes";
+const answer10 = "Signs Point To Yes";
+const answer11 = "Reply Hazy, Try again";
+const answer12 = "Ask Again Later";
+const answer13 = "Better Not Tell You Now";
+const answer14 = "Cannot Predict Now";
+const answer15 = "Concentrate and ask again";
+const answer16 = "Don't count on it";
+const answer17 = "My reply is no";
+const answer18 = "My sources say no";
+const answer19 = "Outlook not so good";
+const answer20 = "Very doubtful";
